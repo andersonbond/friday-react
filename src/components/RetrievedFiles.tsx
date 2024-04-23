91f823b986232d6c5d4c14b98293c4ab1a64463a
Mr. Anderson <anderson.bondoc@gmail.com>
2024-04-23 11:44:38 +0800

    feat: file system

---
+++ src/components/RetrievedFiles.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { REMOTE_API_URL, LOCAL_API_URL } from '../config/properties';
+
+
+const RetrievedFiles: React.FC = () => {
+  const [files, setFiles] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Fetch data from your Node.js API
+    fetch(`${REMOTE_API_URL}/v1/files`)
+      .then(response => response.json())
+      .then(data => setFiles(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); // Empty dependency array to run only once on component mount
+
+  const openFile = (fileName: string) => {
+    // Open the file in a new tab
+    window.open(`${REMOTE_API_URL}/v1/files/${fileName}`, '_blank');
+  };
+
+  const FileCard = (
+    <div className='m-10 p-16'>
+      <h1>📘 Files</h1>
+      <ul className='ol-2 text-18'>
+        {/* Map over the files array to render each file */}
+        {files.map((file, index) => (
+          <li key={index} onClick={() => openFile(file)} style={{ cursor: 'pointer' }}>
+            {file}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className='file-section'>
+      {FileCard}
+    </div>
+  );
+}
+
+export default RetrievedFiles;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,17 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ChatStream from '../components/ChatStream';
 import './Home.css';
 import friday from '../assets/friday.png';
+import RetrievedFiles from '../components/RetrievedFiles';
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <ChatStream />
+        <div className='d-flex'>
+          <ChatStream />
+          <RetrievedFiles />
+        </div>
       </IonContent>
     </IonPage>
   );
