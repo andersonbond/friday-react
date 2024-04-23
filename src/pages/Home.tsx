bcb9e1d602a904f598c551613cbce822fbd76c07
Mr. Anderson <anderson.bondoc@gmail.com>
2024-04-23 20:51:36 +0800

    style: file viewer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonPage } from '@ionic/react';
+
 import ChatStream from '../components/ChatStream';
 import RetrievedFiles from '../components/RetrievedFiles';
 import './Home.css';
-import friday from '../assets/friday.png';
 
 const Home: React.FC = () => {
+  const [showRetrievedFiles, setShowRetrievedFiles] = useState(false); 
+
+  const toggleRetrievedFiles = () => {
+    setShowRetrievedFiles(!showRetrievedFiles);
+  };
+
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className='d-flex'>
-          <ChatStream />
-          <RetrievedFiles />
+          <div>
+            <ChatStream />
+          </div>
+          <div className="retrieved-files-container-lg">
+              <RetrievedFiles />
+          </div>
+          {showRetrievedFiles && (
+            <div className="retrieved-files-container">
+              <RetrievedFiles />
+            </div>
+          )}
+        </div>
+        {/* Burger icon for mobile */}
+        <div className="burger-icon" onClick={toggleRetrievedFiles}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
       </IonContent>
     </IonPage>
